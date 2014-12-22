e0587329e5db9ba51804754bdc461cfb54fe240e
Alexis DEGRYSE <alexis.degryse@ineat-conseil.fr>
2014-12-22 17:11:05 +0100

    css compressed, concat and minify js

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';',
+            },
+            dist: {
+                src: ['assets/js/jquery.fitvids.js', 'assets/js/index.js', 'assets/js/foundation.min.js', 'assets/js/overlay/snap.svg-min.js', 'assets/js/overlay/classie.js', 'assets/js/overlay/demo1.js', 'assets/js/vendor/smoothscroll.js'],
+                dest: 'assets/js/built.js'
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'assets/js/min/built.js': ['assets/js/built.js']
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+};
