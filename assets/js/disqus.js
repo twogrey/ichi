ef09d98c2de93dfd8cda5180593c8d6490a3dece
Bernard Carney <bernard.carney@gmail.com>
2015-04-04 02:36:07 +0000

    Added intial files and code to setup theme variables in one place

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -1,9 +1,22 @@
 var disqus_loaded = false;
 
 function load_disqus() {
-	if (config-disqus-shortname !== '') {
+	if (config_disqus_shortname !== '') {
 		disqus_loaded = true;
-		var disqus_shortname = config-disqus-shortname;
+		
+		// Insert Discussion title and icon
+		// var discussionHead = document.createElement('h3');
+		// discussionHead.className = "title-disqus";
+		// discussionHead.innerHTML = '<span class="fi-comments"></span>Discussion';
+		// document.body.appendChild(discussionHead);
+
+		// Insert Disqus <div> tags
+		// var disqusDiv = document.createElement('div');
+		// disqusDiv.id = "disqus_thread";
+		// document.body.appendChild(disqusDiv);
+
+		// Disqus comments section code
+		var disqus_shortname = config_disqus_shortname;
 		var disqus_identifier = '{{post.id}}'; // make sure to use the post.id as an identifier, otherwise disqus will use the pages url per default, which might be problematic...
 
 		// For embedding disqus under a post
--- assets/js/ichi-config.js
+++ assets/js/ichi-config.js
@@ -1,2 +1,2 @@
 // If you use DisQus comments, add your disqus shortname here
-var config-disqus-shortname = 'barneycarney';
+var config_disqus_shortname = '';
